374f66393d70526f05cc67c968aeebd969912c5d
Sagar Verma <55575131+sagarverma2602@users.noreply.github.com>
2023-10-09 23:15:10 +0530

    login page added

---
+++ src/AdminLogin.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const AdminLogin = () => {
+  return (
+    <>
+    <input type='text' placeholder='Email'/>
+    <br/>
+    <input type='text' placeholder='Password'/>
+    </>
+  )
+}
+
+export default AdminLogin
